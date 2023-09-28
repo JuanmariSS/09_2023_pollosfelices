f801cedec1f543655e679a88fdd103377bbe0004
Jordi <jordi@gmail.com>
2023-09-28 11:11:08 +0200

    se implementan TODOs de ProductoServicesImpl y correspondientes metodos en repositorio

--- src/main/java/com/sinensia/pollosfelices/backend/integration/repositories/ProductoPLRepository.java
+++ src/main/java/com/sinensia/pollosfelices/backend/integration/repositories/ProductoPLRepository.java
@@ -1,11 +1,51 @@
 package com.sinensia.pollosfelices.backend.integration.repositories;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.sinensia.pollosfelices.backend.integration.model.CategoriaPL;
 import com.sinensia.pollosfelices.backend.integration.model.ProductoPL;
 
 @Repository
 public interface ProductoPLRepository extends JpaRepository<ProductoPL, Long>{
 
+	List<ProductoPL> findByPrecioBetween(double min, double max);
+	
+	List<ProductoPL> findByFechaAltaBetween(Date desde, Date hasta);
+	
+	List<ProductoPL> findByDescatalogadoTrue();
+	
+	List<ProductoPL> findByNombreLikeIgnoreCase(String nombre);
+	
+	List<ProductoPL> findByCategoria(CategoriaPL categoria);
+	
+	@Query("UPDATE ProductoPL p SET p.precio = p.precio + (p.precio * :porcentaje / 100) WHERE p IN :productos")
+	@Modifying
+	int incrementarPrecios(List<ProductoPL> productos, double porcentaje);
+	
+	@Query("UPDATE ProductoPL p SET p.precio = p.precio + (p.precio * :porcentaje / 100) WHERE p.codigo IN :codigos")
+	@Modifying
+	int incrementarPrecios(long[] codigos, double porcentaje);
+
+	@Query(value="SELECT C.ID, "
+			   + "       C.NOMBRE, "
+			   + "       COUNT(P.NOMBRE) "
+			   + "FROM   CATEGORIAS C LEFT JOIN PRODUCTOS P ON P.ID_CATEGORIA = C.ID "
+			   + "GROUP BY C.ID "
+			   + "ORDER BY C.ID ", nativeQuery=true)
+	List<Object[]> getEstadisticaNumeroProductos();
+	
+	@Query(value="SELECT C.ID, "
+			   + "       C.NOMBRE, "
+			   + "       AVG(P.PRECIO) "
+			   + "FROM   CATEGORIAS C LEFT JOIN PRODUCTOS P ON P.ID_CATEGORIA = C.ID "
+			   + "GROUP BY C.ID "
+			   + "ORDER BY C.ID ", nativeQuery=true)
+	List<Object[]> getEstadisticaPrecioMedio();
+	
 }
