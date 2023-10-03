c0e1eb409918eb07fa1792657b79657bf0e15727
jordi <rauxasoft@gmail.com>
2023-10-03 08:24:21 +0200

    segunda prueba pull-request

--- src/main/java/com/sinensia/pollosfelices/PollosfelicesApplication.java
+++ src/main/java/com/sinensia/pollosfelices/PollosfelicesApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PollosfelicesApplication {
 
-	// PRUENA PULL-REQUEST
+	// Realizamos PULL-REQUEST!
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PollosfelicesApplication.class, args);
