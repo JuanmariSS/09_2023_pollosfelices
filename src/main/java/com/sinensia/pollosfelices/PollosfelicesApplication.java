f25f32a30e813573fab60e589c0fa029d671bd1b
jordi <rauxasoft@gmail.com>
2023-10-03 08:42:45 +0200

    comentario en main

--- src/main/java/com/sinensia/pollosfelices/PollosfelicesApplication.java
+++ src/main/java/com/sinensia/pollosfelices/PollosfelicesApplication.java
@@ -8,6 +8,7 @@ public class PollosfelicesApplication {
 
 	// Realizamos PULL-REQUEST!
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PollosfelicesApplication.class, args);
 	}
