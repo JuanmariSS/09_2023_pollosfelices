4a62f48ea7a573ef61c0effe1dd96bd03badcb0e
Jordi <jordia4@gmail.com>
2023-10-03 17:35:42 +0200

    listo para practicar con filtros de J2EE en el proyecto Spring Boot con Web

---
+++ src/main/java/com/sinensia/pollosfelices/backend/presentation/config/FiltroInterceptor.java
@@ -0,0 +1,43 @@
+package com.sinensia.pollosfelices.backend.presentation.config;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class FiltroInterceptor implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	
+		request.setAttribute("id", System.currentTimeMillis());
+		
+		chain.doFilter(request, response);
+	
+		HttpServletRequest httpServletRequest = (HttpServletRequest) request; 
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		
+		String ip = httpServletRequest.getRemoteAddr();
+		String path = httpServletRequest.getRequestURL().toString();
+		String method = httpServletRequest.getMethod();
+		
+		System.err.println("REQUEST: " + method + " " + ip + " " + path   + " RESPONSE: " + httpServletResponse.getStatus());
+		
+	}
+	
+	//               REQUEST                                        RESPUESTA
+	// ID      TIMESTAMP   VERBO      IP       RECURSO      STATUS_CODE TIEMPO_RESPUESTA
+	// 10      129344355    GET  10.250.2.3   /productos        200           34
+	// 11      129344399    GET  10.250.2.5   /productos/45     404           17
+	//
+	
+
+}
